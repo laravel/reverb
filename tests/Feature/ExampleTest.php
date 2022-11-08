b1a41470c31897096e5506dabdd19743cd5e088a
Joe Dixon <hello@joedixon.co.uk>
2022-11-08 14:26:32 +0000

    configure tests

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExampleTest()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExampleTest()
+    {
+        $this->assertTrue(true);
+    }
+}
