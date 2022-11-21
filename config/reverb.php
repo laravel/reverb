a66212503c80e0359e0984f70ee70929d6478c48
Joe Dixon <hello@joedixon.co.uk>
2022-11-21 10:13:47 +0000

    add app conifiguration

--- config/reverb.php
+++ config/reverb.php
@@ -23,27 +23,39 @@ return [
     'servers' => [
 
         'ratchet' => [
-
             'host' => env('REVERB_RATCHET_HOST', '127.0.0.1'),
-
             'port' => env('REVERB_RATCHET_PORT', 8080),
-
         ],
 
         'api_gateway' => [
-
             'region' => env('REVERB_API_GATEWAY_REGION', 'us-east-1'),
-
             'endpoint' => env('REVERB_API_GATEWAY_ENDPOINT'),
-
             'connection_cache' => [
-
                 'store' => env('REVERB_CONNECTION_CACHE', 'array'),
-
                 'prefix' => env('REVERB_CONNECTION_CACHE_PREFIX', 'reverb'),
-
             ],
+        ],
 
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reverb Applications
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'apps' => [
+
+        [
+            'id' => env('PUSHER_APP_ID'),
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'capacity' => null,
+            'allowed_origins' => [
+                //
+            ],
         ],
 
     ],
@@ -57,11 +69,8 @@ return [
     */
 
     'channel_cache' => [
-
         'store' => env('REVERB_CHANNEL_CACHE', 'array'),
-
         'prefix' => env('REVERB_CHANNEL_CACHE_PREFIX', 'reverb'),
-
     ],
 
     /*
@@ -73,11 +82,8 @@ return [
     */
 
     'pubsub' => [
-
         'enabled' => env('REVERB_PUBSUB_ENABLED', false),
-
         'channel' => env('REVERB_PUBSUB_CHANNEL', 'reverb'),
-
     ],
 
 ];
