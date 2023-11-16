4409a622a90647bffa815eae4e5ce886595e0d1b
Joe Dixon <hello@joedixon.co.uk>
2023-11-16 20:26:40 +0000

    scaffold routes

--- src/Pusher/Http/Controllers/UsersTerminateController.php
+++ src/Pusher/Http/Controllers/UsersTerminateController.php
@@ -2,26 +2,17 @@
 
 namespace Laravel\Reverb\Pusher\Http\Controllers;
 
-use Laravel\Reverb\Event;
 use Laravel\Reverb\Http\Connection;
 use Psr\Http\Message\RequestInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class EventsController extends Controller
+class UsersTerminateController extends Controller
 {
     /**
      * Handle the request.
      */
     public function handle(RequestInterface $request, Connection $connection, ...$args)
     {
-        $payload = json_decode($this->body, true);
-
-        Event::dispatch($this->application, [
-            'event' => $payload['name'],
-            'channel' => $payload['channel'],
-            'data' => $payload['data'],
-        ]);
-
         return new JsonResponse((object) []);
     }
 }
