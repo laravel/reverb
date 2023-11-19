ef7434a9b57a1247cbe21c6f9c3d6151d7d0bc5a
Joe Dixon <hello@joedixon.co.uk>
2023-11-19 21:18:49 +0000

    implement terminate user endpoint

--- src/Pusher/Http/Controllers/UsersTerminateController.php
+++ src/Pusher/Http/Controllers/UsersTerminateController.php
@@ -5,14 +5,21 @@ namespace Laravel\Reverb\Pusher\Http\Controllers;
 use Laravel\Reverb\Http\Connection;
 use Psr\Http\Message\RequestInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class UsersTerminateController extends Controller
 {
     /**
      * Handle the request.
      */
-    public function handle(RequestInterface $request, Connection $connection, ...$args)
+    public function handle(RequestInterface $request, Connection $connection, ...$args): Response
     {
+        if(! $connection = $this->connections->find($args['user'])) {
+            return new JsonResponse((object) [], 400);
+        }
+
+        $connection->disconnect();
+
         return new JsonResponse((object) []);
     }
 }
---
+++ tests/Feature/Ratchet/UsersTerminateControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Laravel\Reverb\Channels\ChannelBroker;
+use Laravel\Reverb\Tests\RatchetTestCase;
+use React\Http\Message\ResponseException;
+
+use function React\Async\await;
+
+uses(RatchetTestCase::class);
+
+it('returns an error when connection cannot be found', function () {
+    await($this->signedPostRequest('channels/users/not-a-user/terminate_connections'));
+})->throws(ResponseException::class);
+
+it('unsubscribes from all channels and terminates a user', function () {
+    $connection = $this->connect();
+    $this->subscribe('test-channel-one', connection: $connection);
+    $this->subscribe('test-channel-two', connection: $connection);
+
+    $this->subscribe('test-channel-one');
+    $this->subscribe('test-channel-two');
+
+    expect($connections = connectionManager()->all())->toHaveCount(3);
+    expect(channelManager()->connections(ChannelBroker::create('test-channel-one')))->toHaveCount(2);
+    expect(channelManager()->connections(ChannelBroker::create('test-channel-two')))->toHaveCount(2);
+
+    $connection = Arr::first($connections);
+
+    $response = await($this->signedPostRequest("users/{$connection->id()}/terminate_connections"));
+
+    $this->assertSame(200, $response->getStatusCode());
+    $this->assertSame('{}', $response->getBody()->getContents());
+    expect($connections = connectionManager()->all())->toHaveCount(2);
+    expect(channelManager()->connections(ChannelBroker::create('test-channel-one')))->toHaveCount(1);
+    expect(channelManager()->connections(ChannelBroker::create('test-channel-two')))->toHaveCount(1);
+});
