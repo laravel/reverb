898d44d995f2030353cbb974e7e3517baa5cc361
Joe Dixon <hello@joedixon.co.uk>
2023-11-22 17:26:02 +0000

    tidy

--- src/Servers/Reverb/Connection.php
+++ src/Servers/Reverb/Connection.php
@@ -17,11 +17,6 @@ class Connection extends ConnectionContract
     protected ?string $id = null;
 
     /**
-     * The last time the connection was seen.
-     */
-    protected ?string $lastSeenAt = null;
-
-    /**
      * Stores the ping state of the connection.
      *
      * @var \Carbon\Carbon
