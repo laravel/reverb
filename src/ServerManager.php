998a73f9c5183d17e7fdd306f129ab0ddf5d99c8
Joe Dixon <hello@joedixon.co.uk>
2024-01-04 13:53:43 +0000

    remove api gateway support

--- src/ServerManager.php
+++ src/ServerManager.php
@@ -4,7 +4,6 @@ namespace Laravel\Reverb;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\Manager;
-use Laravel\Reverb\Servers\ApiGateway\ApiGatewayProvider;
 use Laravel\Reverb\Servers\Reverb\ReverbProvider;
 
 class ServerManager extends Manager
@@ -33,17 +32,6 @@ class ServerManager extends Manager
         return new ReverbProvider(
             $this->app,
             $this->config->get('reverb.servers.reverb', [])
-        );
-    }
-
-    /**
-     * Creates the API Gateway driver.
-     */
-    public function createApiGatewayDriver(): ApiGatewayProvider
-    {
-        return new ApiGatewayProvider(
-            $this->app,
-            $this->config->get('reverb.servers.api_gateway', [])
         );
     }
 }
