2877752f90b623f6585751a5dbab40fae294caf7
Joe Dixon <joedixon@users.noreply.github.com>
2024-02-29 12:47:58 +0000

    add connections test (#51)

---
+++ tests/Feature/Protocols/Pusher/Reverb/ConnectionsControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Laravel\Reverb\Tests\ReverbTestCase;
+
+use function React\Async\await;
+
+uses(ReverbTestCase::class);
+
+it('can return a connection count', function () {
+    subscribe('test-channel-one');
+    subscribe('presence-test-channel-two');
+
+    $response = await($this->signedRequest('connections'));
+
+    expect($response->getStatusCode())->toBe(200);
+    expect($response->getBody()->getContents())->toBe('{"connections":2}');
+});
+
+it('can return the correct connection count when subscribed to multiple channels', function () {
+    $connection = connect();
+    subscribe('test-channel-one', connection: $connection);
+    subscribe('presence-test-channel-two', connection: $connection);
+
+    $response = await($this->signedRequest('connections'));
+
+    expect($response->getStatusCode())->toBe(200);
+    expect($response->getBody()->getContents())->toBe('{"connections":1}');
+});
