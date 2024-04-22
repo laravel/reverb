52c772cfe74ecc854b11fb3637ee6ac45442039f
Martin Himmel <marty@martyhimmel.me>
2024-04-22 07:01:41 -0400

    Remove unused config lines

--- config/reverb.php
+++ config/reverb.php
@@ -40,11 +40,9 @@ return [
                 'enabled' => env('REVERB_SCALING_ENABLED', false),
                 'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
                 'server' => [
-                    'url' => env('REDIS_URL'),
                     'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'username' => env('REDIS_USERNAME'),
-                    'password' => env('REDIS_PASSWORD'),
                     'port' => env('REDIS_PORT', '6379'),
+                    'password' => env('REDIS_PASSWORD'),
                     'database' => env('REDIS_DB', '0'),
                 ],
             ],
