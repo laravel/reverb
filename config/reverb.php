998e597c4d9327519de01647a7c032022e544f60
Joe Dixon <hello@joedixon.co.uk>
2024-04-24 12:13:50 +0100

    adds redis url support

--- config/reverb.php
+++ config/reverb.php
@@ -40,8 +40,10 @@ return [
                 'enabled' => env('REVERB_SCALING_ENABLED', false),
                 'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
                 'server' => [
+                    'url' => env('REDIS_URL'),
                     'host' => env('REDIS_HOST', '127.0.0.1'),
                     'port' => env('REDIS_PORT', '6379'),
+                    'username' => env('REDIS_USERNAME'),
                     'password' => env('REDIS_PASSWORD'),
                     'database' => env('REDIS_DB', '0'),
                 ],
