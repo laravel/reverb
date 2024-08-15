c22715898dc1df9c6de9028a7c2f7f69b4828cc3
Zeke <erb1995@gmail.com>
2024-08-15 19:26:28 -0300

    Update configuration to match old hardcoded value
    This change makes sure that all future applications that re-publish the configuration keep the original 30 second value instead of the 60 second default configuration. All applications that modified this value in their environment file should not be affected.

--- config/reverb.php
+++ config/reverb.php
@@ -81,7 +81,7 @@ return [
                     'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
                 ],
                 'allowed_origins' => ['*'],
-                'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
+                'ping_interval' => env('REVERB_APP_PING_INTERVAL', 30),
                 'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
             ],
         ],
