8d2551b061553d7218429c42c3e9fe210f56a490
Ashiq <ashiq@square1.io>
2024-09-25 14:41:08 +0600

    add tests

---
+++ tests/Unit/Servers/Reverb/Publishing/RedisPubSubProviderTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use Clue\React\Redis\Client;
+use Laravel\Reverb\Servers\Reverb\Contracts\PubSubIncomingMessageHandler;
+use Laravel\Reverb\Servers\Reverb\Publishing\RedisPubSubProvider;
+use Laravel\Reverb\Servers\Reverb\Publishing\RedisClientFactory;
+use React\EventLoop\LoopInterface;
+
+it('resubscribes to the scaling channel on unsubscribe event', function (){
+
+    $channel = "reverb";
+    $subscribingClient = Mockery::mock(Client::class);
+
+    $subscribingClient->shouldReceive('on')
+        ->with('unsubscribe', Mockery::on(function ($callback) use ($channel) {
+            $callback($channel);
+            return true;
+        }))->once();
+
+    $subscribingClient->shouldReceive('on')
+        ->with('message', Mockery::any())
+        ->zeroOrMoreTimes();
+
+    $subscribingClient->shouldReceive('subscribe')
+        ->twice()
+        ->with($channel);
+
+
+    $clientFactory = Mockery::mock(RedisClientFactory::class);
+
+    // The first call to make() will return a publishing client
+    $clientFactory->shouldReceive('make')
+        ->once()
+        ->andReturn(Mockery::mock(Client::class));
+
+    $clientFactory->shouldReceive('make')
+        ->once()
+        ->andReturn($subscribingClient);
+
+    $provider = new RedisPubSubProvider($clientFactory, Mockery::mock(PubSubIncomingMessageHandler::class), $channel);
+    $provider->connect(Mockery::mock(LoopInterface::class));
+
+    $provider->subscribe();
+});
