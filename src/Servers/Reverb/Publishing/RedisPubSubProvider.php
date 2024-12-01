fbf30e7756b723ba16ba934ffe1930e718a72f8d
Joe Dixon <hello@joedixon.co.uk>
2024-12-01 18:14:44 +0000

    add retries

--- src/Servers/Reverb/Publishing/RedisPubSubProvider.php
+++ src/Servers/Reverb/Publishing/RedisPubSubProvider.php
@@ -2,14 +2,19 @@
 
 namespace Laravel\Reverb\Servers\Reverb\Publishing;
 
+use Clue\React\Redis\Client;
+use Exception;
 use Illuminate\Support\Arr;
 use Illuminate\Support\ConfigurationUrlParser;
 use Illuminate\Support\Facades\Config;
+use Laravel\Reverb\Loggers\Log;
 use Laravel\Reverb\Servers\Reverb\Contracts\PubSubIncomingMessageHandler;
 use Laravel\Reverb\Servers\Reverb\Contracts\PubSubProvider;
 use React\EventLoop\LoopInterface;
+use React\Promise\Deferred;
+use React\Promise\Promise;
 use React\Promise\PromiseInterface;
-use RuntimeException;
+use Throwable;
 
 class RedisPubSubProvider implements PubSubProvider
 {
@@ -17,11 +22,20 @@ class RedisPubSubProvider implements PubSubProvider
 
     protected $subscribingClient;
 
+    protected $publishingClientReconnectionTimer;
+
+    protected $subscribingClientReconnectionTimer;
+
+    protected $queuedSubscriptionEvents = [];
+
+    protected $queuedPublishEvents = [];
+
     public function __construct(
         protected RedisClientFactory $clientFactory,
         protected PubSubIncomingMessageHandler $messageHandler,
         protected string $channel,
-        protected array $server = []
+        protected array $server = [],
+        protected int $reconnectionTimeout = 60
     ) {
         //
     }
@@ -31,8 +45,79 @@ class RedisPubSubProvider implements PubSubProvider
      */
     public function connect(LoopInterface $loop): void
     {
-        $this->publishingClient = $this->clientFactory->make($loop, $this->redisUrl());
-        $this->subscribingClient = $this->clientFactory->make($loop, $this->redisUrl());
+        $this->connectSubcribingClient($loop);
+        $this->connectPublishingClient($loop);
+    }
+
+    protected function connectSubcribingClient($loop)
+    {
+        $this->clientFactory->make($loop, $this->redisUrl())->then(
+            function (Client $client) use ($loop) {
+                $this->subscribingClient = $client;
+                $this->subscribingClientReconnectionTimer = null;
+                $this->configureSubscribingClientErrorHandler($this->subscribingClient, $loop);
+                $this->processQueuedSubscriptionEvents();
+                $this->subscribe();
+                Log::info('Redis subscriber connected');
+            },
+            function (Exception $e) use ($loop) {
+                $this->subscribingClient = null;
+                Log::error($e->getMessage());
+                $this->reconnectSubscribingClient($loop);
+            }
+        );
+    }
+
+    protected function configureSubscribingClientErrorHandler(Client $client, LoopInterface $loop)
+    {
+        $client->on('close', function () use ($loop) {
+            $this->subscribingClient = null;
+            Log::info('Redis subscriber disconnected');
+            $this->reconnectSubscribingClient($loop);
+        });
+    }
+
+    protected function reconnectSubscribingClient(LoopInterface $loop)
+    {
+        $this->subscribingClientReconnectionTimer = $loop->addTimer(1, function () use ($loop) {
+            Log::info('Attempting to reconnect Redis subscriber');
+            $this->connectSubcribingClient($loop);
+        });
+    }
+
+    protected function connectPublishingClient($loop)
+    {
+        $this->clientFactory->make($loop, $this->redisUrl())->then(
+            function (Client $client) use ($loop) {
+                $this->publishingClient = $client;
+                $this->publishingClientReconnectionTimer = null;
+                $this->configurePublishingClientErrorHandler($this->publishingClient, $loop);
+                $this->processQueuedPublishEvents();
+                Log::info('Redis publisher connected');
+            },
+            function (Exception $e) use ($loop) {
+                $this->publishingClient = null;
+                Log::error($e->getMessage());
+                $this->reconnectPublishingClient($loop);
+            }
+        );
+    }
+
+    protected function configurePublishingClientErrorHandler(Client $client, LoopInterface $loop)
+    {
+        $client->on('close', function () use ($loop) {
+            $this->publishingClient = null;
+            Log::info('Redis publisher disconnected');
+            $this->reconnectPublishingClient($loop);
+        });
+    }
+
+    protected function reconnectPublishingClient(LoopInterface $loop)
+    {
+        $this->publishingClientReconnectionTimer = $loop->addTimer(1, function () use ($loop) {
+            Log::info('Attempting to reconnect Redis publisher');
+            $this->connectPublishingClient($loop);
+        });
     }
 
     /**
@@ -49,8 +134,13 @@ class RedisPubSubProvider implements PubSubProvider
      */
     public function subscribe(): void
     {
-        $this->ensureConnected();
+        if (! $this->clientIsReady($this->subscribingClient)) {
+            $this->queueSubscriptionEvent('subscribe');
 
+            return;
+        }
+
+        Log::info('Subscribing');
         $this->subscribingClient->subscribe($this->channel);
 
         $this->subscribingClient->on('message', function (string $channel, string $payload) {
@@ -69,6 +159,14 @@ class RedisPubSubProvider implements PubSubProvider
      */
     public function on(string $event, callable $callback): void
     {
+        dump($event, $callback);
+
+        if (! $this->clientIsReady($this->subscribingClient)) {
+            $this->queueSubscriptionEvent('on', [$event => $callback]);
+        }
+
+        dump($event, $callback);
+
         $this->subscribingClient->on('message', function (string $channel, string $payload) use ($event, $callback) {
             $payload = json_decode($payload, associative: true, flags: JSON_THROW_ON_ERROR);
 
@@ -79,11 +177,18 @@ class RedisPubSubProvider implements PubSubProvider
     }
 
     /**
-     * Publish a payload to the publisher.
+     * Publish a payload to the publishingClientReconnectionTimer.
      */
     public function publish(array $payload): PromiseInterface
     {
-        $this->ensureConnected();
+        Log::info('Sending');
+        if (! $this->clientIsReady($this->publishingClient)) {
+            $this->queuePublishEvent($payload);
+
+            return new Promise(fn () => new Exception('It\'s broken'));
+        }
+
+        Log::info('Publishing');
 
         return $this->publishingClient->publish($this->channel, json_encode($payload));
     }
@@ -127,13 +232,41 @@ class RedisPubSubProvider implements PubSubProvider
         return "redis{$protocol}://{$host}:{$port}".($query ? "?{$query}" : '');
     }
 
-    /**
-     * Ensure that a connection to Redis has been established.
-     */
-    protected function ensureConnected(): void
+    protected function queueSubscriptionEvent(): void
     {
-        if (! $this->publishingClient) {
-            throw new RuntimeException('Connection to Redis has not been established.');
+        $this->queuedSubscriptionEvents['subscribe'] = true;
+    }
+
+    protected function queuePublishEvent(array $payload): void
+    {
+        $this->queuedPublishEvents[] = $payload;
+    }
+
+    protected function clientIsReady(mixed $client): bool
+    {
+        return (bool) $client === true && $client instanceof Client;
+    }
+
+    protected function processQueuedSubscriptionEvents(): void
+    {
+        dump($this->queuedSubscriptionEvents);
+        foreach ($this->queuedSubscriptionEvents as $event => $args) {
+            match ($event) {
+                'subscribe' => $this->subscribe(),
+                'on' => $this->on(...$args),
+                default => null
+            };
+
         }
+        $this->queuedSubscriptionEvents = [];
+    }
+
+    protected function processQueuedPublishEvents(): void
+    {
+        dump($this->queuedPublishEvents);
+        foreach ($this->queuedPublishEvents as $event) {
+            $this->publish($event);
+        }
+        $this->queuedPublishEvents = [];
     }
 }
