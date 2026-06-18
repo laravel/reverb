0f8c9d813198465b3391b33a501d6cd3c2d5401e
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-06-18 12:54:05 -0400

    add helper method for register dev commands

---
+++ src/Reverb.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Laravel\Reverb;
+
+class Reverb
+{
+
+    /**
+     * Helper method to register the Reverb development server Artisan command when running in a local environment.
+     * Should be called from the `boot` method of an application service provider.
+     *
+     * @return void
+     */
+    public static function registerDevCommands(): void
+    {
+        if (class_exists(\Illuminate\Foundation\DevCommands::class)) {
+            \Illuminate\Foundation\DevCommands::artisan('reverb:start', 'reverb');
+        }
+    }
+}
