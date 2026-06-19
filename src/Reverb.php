53fb53bad5d76c16e3ddbea72fa3ba9a93a6b6f0
taylorotwell <463230+taylorotwell@users.noreply.github.com>
2026-06-19 13:45:43 +0000

    Fix code styling

--- src/Reverb.php
+++ src/Reverb.php
@@ -2,17 +2,17 @@
 
 namespace Laravel\Reverb;
 
+use Illuminate\Foundation\DevCommands;
+
 class Reverb
 {
     /**
      * Register the Reverb dev commands.
-     *
-     * @return void
      */
     public static function registerDevCommands(): void
     {
-        if (class_exists(\Illuminate\Foundation\DevCommands::class)) {
-            \Illuminate\Foundation\DevCommands::artisan('reverb:start', 'reverb');
+        if (class_exists(DevCommands::class)) {
+            DevCommands::artisan('reverb:start', 'reverb');
         }
     }
 }
